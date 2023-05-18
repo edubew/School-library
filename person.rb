406d07be193c06d404de9d7ad50aaed13ffe9dd8
edubew <edubew98@gmail.com>
2023-05-18 09:06:52 +0300

    Turn Person class to inherit from Nameable class

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require_relative './nameable'
+
+class Person < Nameable
   def initialize(age, name: 'Unknown', parent_permission: true)
     @id = rand(1..100)
     @age = age
@@ -8,6 +10,10 @@ class Person
 
   attr_reader :id
   attr_accessor :name, :age
+
+  def correct_name
+    @name
+  end
 
   # Public method to return true if the person is of age or has permission from parents
   def can_use_services?
