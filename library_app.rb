9252d7750d678bcf10d8cde868388b3640aff6f0
edubew <edubew98@gmail.com>
2023-05-23 03:36:11 +0300

    Create a class to handle library related actions

---
+++ library_app.rb
@@ -0,0 +1,29 @@
+class LibraryApp
+  def initialize
+    @app = App.new
+  end
+
+  def list_all_books
+    @app.list_all_books
+  end
+
+  def list_all_people
+    @app.list_all_people
+  end
+
+  def create_person
+    @app.create_person
+  end
+
+  def create_book
+    @app.create_book
+  end
+
+  def create_rental
+    @app.create_rental
+  end
+
+  def list_rentals
+    @app.list_rentals
+  end
+end
